97d029389503ecf9a47c7b8d1172d3423859f399
Mik-C <miquelclark@hotmail.com>
2016-07-12 19:36:25 +0200

    Testeado en localhost
    - Testeo de todas las funcionalidades menos permisos y multiples
    usuarios en localhost.
    - Arreglo de errores.

--- src/com/mik_c/chiselplugin/ChiselListener.java
+++ src/com/mik_c/chiselplugin/ChiselListener.java
@@ -7,26 +7,19 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
 
 
 public class ChiselListener implements Listener{
-	@SuppressWarnings("deprecation")
-	@EventHandler(priority=EventPriority.HIGH)
-	public void onBlockBreak(PlayerInteractEvent event){
-		if(ChiselPlugin.pconfig.containsKey(event.getPlayer().getUniqueId())){
-			if(event.getItem().getTypeId() == ChiselPlugin.pconfig.get(event.getPlayer().getUniqueId()).getItem()){
-				event.setCancelled(true);
-			}
-		}
-	}
-	
 	@SuppressWarnings("deprecation")
 	@EventHandler(priority=EventPriority.HIGH)
 	public void onPlayerUse(PlayerInteractEvent event){
 		if(ChiselPlugin.pconfig.containsKey(event.getPlayer().getUniqueId())){				//If the player has a config
 			PlayerConfig pc = ChiselPlugin.pconfig.get(event.getPlayer().getUniqueId());	//Get de config
 			Player p = event.getPlayer();
-			if(pc.getItem() == event.getItem().getTypeId()){								//If the item is a chisel
+			ItemStack is = event.getItem();
+			if(is == null) return;
+			if(pc.getItem().equals(is.getTypeId())){										//If the item is a chisel
 				event.setCancelled(true);													//Don't use the item if it's a chisel for that player
 				if(event.getAction().equals(Action.LEFT_CLICK_BLOCK)){
 					ChiselWork.setSelection(event);
